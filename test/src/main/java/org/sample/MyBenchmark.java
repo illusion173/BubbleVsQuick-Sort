9c2fc728cea1f3ed0640b0db4e2a7de582c26f08
Jeremiah Webb <illusion173@hotmail.com>
2022-11-04 22:11:53 -0400

    Updated Results

--- test/src/main/java/org/sample/MyBenchmark.java
+++ test/src/main/java/org/sample/MyBenchmark.java
@@ -38,7 +38,6 @@ class Bubble_Sort_Class {
 		for (int i = 0; i < n - 1; i++)
 			for (int j = 0; j < n - i - 1; j++)
 				if (array[j] > array[j + 1]) {
-					// swap arr[j+1] and arr[j]
 					int temp = array[j];
 					array[j] = array[j + 1];
 					array[j + 1] = temp;
